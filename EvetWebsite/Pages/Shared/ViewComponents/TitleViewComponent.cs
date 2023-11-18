a2491e0b84ddb2ce0234d238f36c39738d0f42cc
VS-GitHub-001 <105536514+VS-GitHub-001@users.noreply.github.com>
2023-11-18 08:30:02 +0100

    ll

---
+++ EvetWebsite/Pages/Shared/ViewComponents/TitleViewComponent.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EvetWebsite.Pages.Shared.ViewComponents
+{
+    
+    public class TitleViewComponent : ViewComponent
+    {
+
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public TitleViewComponent(
+            RoleManager<IdentityRole> roleManager
+            )
+        {
+
+            _roleManager = roleManager;
+        }
+
+        public IdentityRole RoleData { get; set; }
+
+        public async Task<IViewComponentResult> InvokeAsync(string name)
+        {
+            if (name.Contains("Pre Order"))
+            {
+                TempData["fullname"] = "pppppp";
+            }
+            else
+            {
+                TempData["fullname"] = "Home";
+            }
+            
+            return View( );
+        }
+    }
+}
